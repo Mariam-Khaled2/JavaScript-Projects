383edef6046527a77297f2ee710c9e865a9122d6
Mariam Khaled <mariamkhaled2181@gmail.com>
2026-09-01 20:13:55 +0300

    Implement basic calculator functionality

---
+++ Calculator /main.js
@@ -0,0 +1,45 @@
+const calculatorContainer = document.getElementById('calculator_container');
+const displayArea = document.getElementById('display-area');
+
+calculatorContainer.addEventListener('click', e => {
+   if (e.target.nodeName === 'BUTTON') {
+    switch (e.target.textContent) {
+        case 'C':
+            clear();
+            break;
+            case "DEL":
+                deleteOneValue();
+                break;
+        case '=':
+            calculate();
+            break;
+            default:
+                addToDisplayArea(e.target.textContent);
+                break;
+    }
+    }
+        }) 
+
+
+function clear() {
+    displayArea.textContent = '';
+}
+
+function addToDisplayArea(value) {
+    displayArea.textContent += value;
+}
+
+function deleteOneValue() {
+    let currentContent = displayArea.textContent;
+    displayArea.textContent = currentContent.substring(0, currentContent.length - 1);
+}
+
+function calculate() {
+    try {
+        let calculation = math.evaluate(displayArea.textContent);
+        displayArea.textContent = calculation;
+    } catch (error) {
+        displayArea.textContent = 'Invalid Operation';
+        console.error(error);
+    }
+}
